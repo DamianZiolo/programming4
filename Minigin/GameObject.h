614fbfe1daf4f20efced695890353218bc2af368
Damian Ziolo <damian.ziolo@student.howest.be>
2026-02-18 12:36:14 +0100

    GetComponent And HasComponent + AddComponentFix
    Make GameObject final and add component management helpers. Introduces GetComponent<T>() and HasComponent<T>() (using dynamic_cast) and prevents adding duplicate components in the templated AddComponent  if present. Adds static_assert checks (requires <type_traits>), moves and initializes m_components. TextObject cause errors now but I will fix it in nextCommit by replacing TextObject with TextComponent

--- Minigin/GameObject.h
+++ Minigin/GameObject.h
@@ -4,24 +4,23 @@
 #include "Transform.h"
 #include "Component.h"
 #include <vector>
+#include <type_traits>
 
 
 namespace dae
 {
 	class Texture2D;
-	class GameObject 
+	class GameObject final
 	{
 		Transform m_transform{};
 		std::shared_ptr<Texture2D> m_texture{};
+		std::vector<std::unique_ptr<Component>> m_components{};
 	public:
 		virtual void Update();
 		virtual void Render() const;
 
 		void SetTexture(const std::string& filename);
 		void SetPosition(float x, float y);
-
-		std::vector<std::unique_ptr<Component>> m_components{};
-
 		//---Componennt interface---
 		//Variadic template https://www.geeksforgeeks.org/cpp/variadic-function-templates-c/
 		template<typename T, typename... Args> // T -> component that I want to add, Args... -> arguments that I want to pass to the constructor of the component, I use variadic templates because I want to be able to pass any number of arguments to the constructor of the component, and I use perfect forwarding because I want to be able to pass lvalues and rvalues to the constructor of the component, and I want to avoid unnecessary copies of the arguments
@@ -30,6 +29,12 @@ namespace dae
 		{
 			static_assert(std::is_base_of_v<Component, T>, "T must derive from Component");
 			//std::forward always keep the same category, so for example with renderTexturecomponent if I pass ,,Texture.txt" as lvalue it's lvalue, but if I pass variable path then it's r value
+			T* existingComponent = GetComponent<T>();
+			if (existingComponent != nullptr)
+			{
+				return existingComponent;
+			}
+			
 			auto component = std::make_unique<T>(this, std::forward<Args>(args)...);
 			m_components.emplace_back(std::move(component)); //I move the unique pointer to the vector, because I want the vector to take ownership of the component
 		}
@@ -39,10 +44,40 @@ namespace dae
 		{
 			static_assert(std::is_base_of_v<Component, T>, "T must derive from Component");
 			if (!component) return;
-			if (component->GetOwner() != this) return; // albo assert
+			if (component->GetOwner() != this) return; //I don't want to be able to delete component from other GO
 			component->m_markedForRemoval = true;
 		}
 
+		template<typename T>
+		T* GetComponent()
+		{
+			static_assert(std::is_base_of_v<Component, T>, "T must derive from Component");
+			for (auto& component : m_components)
+			{
+				//for every component I try to cast it to the type T, if cast success I return it
+				T* casted = dynamic_cast<T*>(component.get());
+				if (casted != nullptr)
+				{
+					return casted;
+				}
+			}
+			return nullptr;
+		}
+
+		template<typename T>
+		bool HasComponent() const
+		{
+			// GetComponent is not const, but I want to be able to call it from a const function
+			GameObject* ConstThis = const_cast<GameObject*>(this);
+			T* component = ConstThis->GetComponent<T>();
+
+			if (component != nullptr)
+			{
+				return true;
+			}
+
+			return false;
+		}
 
 		void CleanupRemovedComponents();
 

0682ba9e0c2f1528e30036b3b6dfb40abcfe048d
Damian Ziolo <damian.ziolo@student.howest.be>
2026-03-02 16:13:35 +0100

    Fixed variable in RotationCOmponent

--- Minigin/RotationComponent.h
+++ Minigin/RotationComponent.h
@@ -17,10 +17,9 @@ namespace dae
 
 	private:
 		float m_radius{ 0.0f };
-		float m_radius{ 0.0f };
-		bool m_clockwise{ false };
-		float m_radius{ 0.0f };
-		float m_phaseRad{ 0.0f }; //radians - where is object on the circle at the moment (0 means to the right of the orbit point, PI/2 means above the orbit point)
-		glm::vec3 center{ 0.0f, 0.0f, 0.0f };
+		float m_angularSpeed{ 0.0f };
+		bool  m_clockwise{ false };
+		float m_phaseRad{ 0.0f }; // radians
+		glm::vec3 m_center{ 0.0f, 0.0f, 0.0f };
 	};
 }

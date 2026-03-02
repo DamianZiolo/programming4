c920028519f7a9420ae98f8e979c27ef6e099d6e
Damian Ziolo <damian.ziolo@student.howest.be>
2026-03-02 16:05:13 +0100

    Changed order of variable to allow webbuild

--- Minigin/RotationComponent.h
+++ Minigin/RotationComponent.h
@@ -16,9 +16,10 @@ namespace dae
 		void Update() override;
 
 	private:
+		float m_radius{ 0.0f };
+		float m_radius{ 0.0f };
 		bool m_clockwise{ false };
 		float m_radius{ 0.0f };
-		float m_angularSpeed{ 0.0f };  //radians
 		float m_phaseRad{ 0.0f }; //radians - where is object on the circle at the moment (0 means to the right of the orbit point, PI/2 means above the orbit point)
 		glm::vec3 center{ 0.0f, 0.0f, 0.0f };
 	};

c4e8b9379c4937fb226be3809c87f9b1f76b139b
DamianZiolo <damian.ziolo@student.howest.be>
2026-03-11 23:15:22 +0100

    Emscripten implementation with SDL

--- Minigin/Controller.cpp
+++ Minigin/Controller.cpp
@@ -1,33 +1,51 @@
 #include "Controller.h"
 
+#if __EMSCRIPTEN__
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_gamepad.h>
+#else
 #include <Windows.h>
 #include <Xinput.h>
+#endif
+
+#include <array>
+#include <memory>
 
 namespace
 {
+#if __EMSCRIPTEN__
+	SDL_GamepadButton ToSDLButton(dae::ControllerButton button)
+	{
+		switch (button)
+		{
+		case dae::ControllerButton::DPadUp:    return SDL_GAMEPAD_BUTTON_DPAD_UP;
+		case dae::ControllerButton::DPadDown:  return SDL_GAMEPAD_BUTTON_DPAD_DOWN;
+		case dae::ControllerButton::DPadLeft:  return SDL_GAMEPAD_BUTTON_DPAD_LEFT;
+		case dae::ControllerButton::DPadRight: return SDL_GAMEPAD_BUTTON_DPAD_RIGHT;
+		case dae::ControllerButton::A:         return SDL_GAMEPAD_BUTTON_SOUTH;
+		case dae::ControllerButton::B:         return SDL_GAMEPAD_BUTTON_EAST;
+		case dae::ControllerButton::X:         return SDL_GAMEPAD_BUTTON_WEST;
+		case dae::ControllerButton::Y:         return SDL_GAMEPAD_BUTTON_NORTH;
+		default:                              return SDL_GAMEPAD_BUTTON_INVALID;
+		}
+	}
+#else
 	WORD ToXInputButton(dae::ControllerButton button)
 	{
 		switch (button)
 		{
-		case dae::ControllerButton::DPadUp:    
-			return XINPUT_GAMEPAD_DPAD_UP;
-		case dae::ControllerButton::DPadDown:  
-			return XINPUT_GAMEPAD_DPAD_DOWN;
-		case dae::ControllerButton::DPadLeft:  
-			return XINPUT_GAMEPAD_DPAD_LEFT;
-		case dae::ControllerButton::DPadRight: 
-			return XINPUT_GAMEPAD_DPAD_RIGHT;
-		case dae::ControllerButton::A:         
-			return XINPUT_GAMEPAD_A;
-		case dae::ControllerButton::B:         
-			return XINPUT_GAMEPAD_B;
-		case dae::ControllerButton::X:         
-			return XINPUT_GAMEPAD_X;
-		case dae::ControllerButton::Y:         
-			return XINPUT_GAMEPAD_Y;
-		default: return 0;
+		case dae::ControllerButton::DPadUp:    return XINPUT_GAMEPAD_DPAD_UP;
+		case dae::ControllerButton::DPadDown:  return XINPUT_GAMEPAD_DPAD_DOWN;
+		case dae::ControllerButton::DPadLeft:  return XINPUT_GAMEPAD_DPAD_LEFT;
+		case dae::ControllerButton::DPadRight: return XINPUT_GAMEPAD_DPAD_RIGHT;
+		case dae::ControllerButton::A:         return XINPUT_GAMEPAD_A;
+		case dae::ControllerButton::B:         return XINPUT_GAMEPAD_B;
+		case dae::ControllerButton::X:         return XINPUT_GAMEPAD_X;
+		case dae::ControllerButton::Y:         return XINPUT_GAMEPAD_Y;
+		default:                               return 0;
 		}
 	}
+#endif
 }
 
 namespace dae
@@ -40,8 +58,47 @@ namespace dae
 		{
 		}
 
+#if __EMSCRIPTEN__
+		~Impl()
+		{
+			if (m_pGamepad)
+			{
+				SDL_CloseGamepad(m_pGamepad);
+				m_pGamepad = nullptr;
+			}
+		}
+#endif
+
 		void Update()
 		{
+#if __EMSCRIPTEN__
+			for (int i = 0; i < static_cast<int>(SDL_GAMEPAD_BUTTON_COUNT); ++i)
+			{
+				m_PreviousButtons[i] = m_CurrentButtons[i];
+			}
+
+			OpenGamepadIfNeeded();
+
+			if (!m_pGamepad)
+			{
+				for (int i = 0; i < static_cast<int>(SDL_GAMEPAD_BUTTON_COUNT); ++i)
+				{
+					m_CurrentButtons[i] = false;
+				}
+				m_IsConnected = false;
+				return;
+			}
+
+			m_IsConnected = true;
+
+			for (int i = 0; i < static_cast<int>(SDL_GAMEPAD_BUTTON_COUNT); ++i)
+			{
+				m_CurrentButtons[i] = SDL_GetGamepadButton(
+					m_pGamepad,
+					static_cast<SDL_GamepadButton>(i)
+				);
+			}
+#else
 			m_PreviousState = m_CurrentState;
 			ZeroMemory(&m_CurrentState, sizeof(XINPUT_STATE));
 
@@ -56,37 +113,98 @@ namespace dae
 				return;
 			}
 
-			const auto buttonChanges = m_CurrentState.Gamepad.wButtons ^ m_PreviousState.Gamepad.wButtons;
-			m_ButtonsPressedThisFrame = buttonChanges & m_CurrentState.Gamepad.wButtons;
-			m_ButtonsReleasedThisFrame = buttonChanges & (~m_CurrentState.Gamepad.wButtons);
+			const auto buttonChanges =
+				m_CurrentState.Gamepad.wButtons ^ m_PreviousState.Gamepad.wButtons;
+
+			m_ButtonsPressedThisFrame =
+				buttonChanges & m_CurrentState.Gamepad.wButtons;
+
+			m_ButtonsReleasedThisFrame =
+				buttonChanges & (~m_CurrentState.Gamepad.wButtons);
+#endif
 		}
 
 		bool IsPressed(ControllerButton button) const
 		{
+#if __EMSCRIPTEN__
+			const auto sdlButton = ToSDLButton(button);
+			if (sdlButton == SDL_GAMEPAD_BUTTON_INVALID)
+				return false;
+
+			return m_CurrentButtons[static_cast<int>(sdlButton)];
+#else
 			const WORD xButton = ToXInputButton(button);
 			return (m_CurrentState.Gamepad.wButtons & xButton) != 0;
+#endif
 		}
 
 		bool IsDownThisFrame(ControllerButton button) const
 		{
+#if __EMSCRIPTEN__
+			const auto sdlButton = ToSDLButton(button);
+			if (sdlButton == SDL_GAMEPAD_BUTTON_INVALID)
+				return false;
+
+			const int index = static_cast<int>(sdlButton);
+			return !m_PreviousButtons[index] && m_CurrentButtons[index];
+#else
 			const WORD xButton = ToXInputButton(button);
 			return (m_ButtonsPressedThisFrame & xButton) != 0;
+#endif
 		}
 
 		bool IsUpThisFrame(ControllerButton button) const
 		{
+#if __EMSCRIPTEN__
+			const auto sdlButton = ToSDLButton(button);
+			if (sdlButton == SDL_GAMEPAD_BUTTON_INVALID)
+				return false;
+
+			const int index = static_cast<int>(sdlButton);
+			return m_PreviousButtons[index] && !m_CurrentButtons[index];
+#else
 			const WORD xButton = ToXInputButton(button);
 			return (m_ButtonsReleasedThisFrame & xButton) != 0;
+#endif
 		}
 
 	private:
+#if __EMSCRIPTEN__
+		void OpenGamepadIfNeeded()
+		{
+			if (m_pGamepad)
+				return;
+
+			int count = 0;
+			SDL_JoystickID* gamepads = SDL_GetGamepads(&count);
+
+			if (!gamepads)
+			{
+				return;
+			}
+
+			if (m_ControllerIndex < count)
+			{
+				m_pGamepad = SDL_OpenGamepad(gamepads[m_ControllerIndex]);
+			}
+
+			SDL_free(gamepads);
+		}
+#endif
+
 		int m_ControllerIndex{};
+		bool m_IsConnected{ false };
+
+#if __EMSCRIPTEN__
+		SDL_Gamepad* m_pGamepad{ nullptr };
+		std::array<bool, SDL_GAMEPAD_BUTTON_COUNT> m_PreviousButtons{};
+		std::array<bool, SDL_GAMEPAD_BUTTON_COUNT> m_CurrentButtons{};
+#else
 		XINPUT_STATE m_PreviousState{};
 		XINPUT_STATE m_CurrentState{};
-
 		int m_ButtonsPressedThisFrame{};
 		int m_ButtonsReleasedThisFrame{};
-		bool m_IsConnected{ false };
+#endif
 	};
 
 	Controller::Controller(int controllerIndex)
@@ -115,5 +233,4 @@ namespace dae
 	{
 		return m_pImpl->IsUpThisFrame(button);
 	}
-
 }
